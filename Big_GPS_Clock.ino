e7c8ac9f969ac2f15c41c34e80c52254d6cd4d72
hindlx <ljlindh@gmail.com>
2017-03-28 20:56:49 +0200

    Update Big_GPS_Clock.ino

--- Big_GPS_Clock.ino
+++ Big_GPS_Clock.ino
@@ -1,28 +1,29 @@
 /*
  * Big_GPS_Clock.ino
- *
- * Yet another GPS syncronized clock built on Ardunino and u-blox GPS module.
- * Use MAX7219 one chip for displaying time and optional another for some status
+ * by Lars-Johan Lindh
+ * Yet another GPS syncronised clock, built on Ardunino mini and u-blox GPS module.
+ * Use MAX7219 one 7-segment display for time and optional display for status
  * messages.
  *
- * Set TZ
- * DST calculations are for Sweden
+ * DP (digit 3 and 4) for colon "blink".
+ * Use DP (digit 1) for indication of many > 3 satelites in sight, means very high time accuracy.
  *
- *
- * Copyright (C) 2017 Lars-Johan Lindh
+ * GPS u-blox 8, software version 14.
+ * Set TZ for timezone adjustments against UTC-time. 
+ * DST calculations are for Sweden, last sunday in mars and october.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  *
  *      1          a
  *    -----      -----        -----      -----     --   --
@@ -38,30 +39,28 @@
  * Dig  H          H     c      M          M        S    S
  *
  */
+#define Version   "170328"        // Softvare version / date
+#define displays  1               // # of displays
+#define DEBUG     0               // Prints time and date to serial port when DEBUG = 1
 
+const int     TZ  = 1;            // GPS time = UTC, set TZ according to time zone
+int           DST = 0;            // Daylight Saving Time start variable set
+float         LightSensorValue;   // LDR-value
 
 // Dependensis / Librarys
-// #include "avr/wdt.h"             // Built in watchdog
+#include "avr/wdt.h"              // Built in watchdog
 
 #include <LedControl.h>
-LedControl lc = LedControl(10, 12, 11, 2);
-/* pin 11 is connected to the CS (CS)(LOAD)
- * pin 12 is connected to the CLK (CLK)
- * pin 10 is connected to LOAD(DIN)  */
-
-#include <Time.h>
-#include <AltSoftSerial.h>
-// Connect the GPS RX/TX to arduino pins 8 and 9
+LedControl lc = LedControl(12, 11, 13, displays);
+ /* pin 11 is connected to the CLK (CLK)
+  * pin 12 is connected to LOAD(DIN)
+  * pin 13 is connected to the CS (CS)(LOAD)
+  */
+#include <Time.h>                 // Use Time lib for time adjusting
+#include <AltSoftSerial.h>        // Connect the GPS RX/TX to arduino mini pins 8 and 9, default AltSoftSerial pin
 AltSoftSerial serial ;
 
-#define Version "170223"         // Softvare version = releasedate
-
-const int     TZ  = 1;           // GPS time = UTC/GMT set TZ according to time zone
-int           DST = 0;           // Daylight Saving Time start variable
-float         LightSensorValue;  // LDR-value
-boolean       DEBUG = 1;         // Prints time and date to serial port when DEBUG = 1
-
-time_t prevDisplay = 0;          // When the digital clock was displayed last
+time_t prevDisplay = 0;           // When the digital clock was displayed last
 
 const char UBLOX_INIT[] PROGMEM = {
   // Disable NMEA http://www.iforce2d.net/
@@ -90,7 +89,7 @@ const char UBLOX_INIT[] PROGMEM = {
 
 const unsigned char UBX_HEADER[] = { 0xB5, 0x62 };
 
-struct NAV_PVT {
+struct NAV_PVT {                  // u-Blox 8 version 14
   unsigned char  cls;
   unsigned char  id;
   unsigned short len;
@@ -140,14 +139,12 @@ void setup() {
   Serial.println("Booting GPS clock ");
   Serial.print("Software version: 20");
   Serial.println(Version);
-  lc.shutdown(0, false);         // Bring MAX72XX out off power-saving mode
-  lc.setIntensity(0, 0);         // Set the brightness to startvalue
-  lc.clearDisplay(0);            // Clear the display
-  delay(100);
-  lc.shutdown(1, false);         // Bring MAX72XX out off power-saving mode
-  lc.setIntensity(1, 0);         // Set the brightness to startvalue
-  lc.clearDisplay(1);            // Clear the display
-
+  for (int z = 0; z < displays; z++) {
+    lc.shutdown(z, false);        // Bring MAX72XX out off power-saving mode
+    lc.setIntensity(z, 5);        // Set the brightness to startvalue
+    lc.clearDisplay(z);           // Clear the display
+    delay(100);
+  }
   /* start softSerial and send configuration data in UBX protocol
    * see http://www.iforce2d.net/ */
   Serial.print(" Configuration of GPS ...");
@@ -159,14 +156,16 @@ void setup() {
   }
   Serial.println(".");
   Serial.println("GPS ready");
-  write_msg(4);                  // Lights all led in disply for visual check of LEDs
-  write_msg(2);                  // Write software version
-  write_msg(3);                  // Lights all led in disply for visual check of LEDs
-  write_msg(1);                  // Write HELO to display
-  //  wdt_enable(WDTO_8S);           // Start Arduino Watchdog and set Threshold value
-  //  Serial.println(" Arduino watchdog activated!!");
+  write_msg(3);                   // Lights all led in disply for visual check of LEDs
+  write_msg(2);                   // Write software version
+//  write_msg(4);                   // Lights all led in disply for visual check of LEDs
+//  write_msg(1);                   // Write HELO to display
+  wdt_enable(WDTO_8S);            // Start Arduino Watchdog and set Threshold value
+  Serial.println(" Arduino watchdog activated!!");
 }
-
+/* Main loop
+ * 
+ */
 void loop() {
   if ( processGPS() ) {
     setTime(pvt.hour, pvt.minute, pvt.second, pvt.day, pvt.month, pvt.year);
@@ -175,16 +174,17 @@ void loop() {
       if (now() != prevDisplay) {                  // update the display only if the time has changed
         prevDisplay = now();
         digitalClockDisplay();
-        DST = calcDST();                           // Check DST
+        DST = calcDST();                           // Check DST, Yes one sec after
       }
-      //      wdt_reset();                                 // Reset the watchdog otherwise the arduino restarts
+      wdt_reset();                                 // Reset the watchdog otherwise the arduino restarts
     }
   }
 }
 
 /* Calculate when summertime in sweden
  * PARAMS:
- * RETURN: Return DST set to 0 or 1 */
+ * RETURN: Return DST set to 0 or 1 
+ */
 int calcDST() {
   if (month() > 3 and month() < 10) {              // Summer months 4,5,6,7,8,9
     return 1;
@@ -208,7 +208,8 @@ int calcDST() {
  * GPS-fix value, Day Of Week and light sensor value to display #2
  * Blink : if correct time othervise ether ' or . is light
  * PARAMS: -
- * RETURN: - */
+ * RETURN: -
+ */
 void digitalClockDisplay() {
   WriteDisp(second(), 0, 1);
   if ( bitRead(pvt.valid, 0) == 1 ) {              // Check if received time from GPS is okay, then show time
@@ -216,39 +217,47 @@ void digitalClockDisplay() {
     WriteDisp(hour(), 0, 5);
   }
   else {
-    write_msg(0);                                   // write Err to display to indicate GPS time lost
+    write_msg(0);                                  // write Err to display to indicate GPS time lost
   }
-  if ( bitRead(pvt.valid, 2) == 1 ) {               // Check if valid time is received from GPS
+  if ( bitRead(pvt.valid, 2) == 1 ) {              // Check if valid time is received from GPS
     if ( (pvt.second & 0x01) == 0) {
-      lc.setLed(0, 0, 0, false);                    // Blink ':' off every 2 second
+      lc.setLed(0, 3, 0, false);                   // Use DP for Blink ' ' off every 2 second
       lc.setLed(0, 4, 0, false);
     }
     else {
-      lc.setLed(0, 0, 0, true);                     // Blink ':' on every 2 second
+      lc.setLed(0, 3, 0, true);                    // Use DP for Blink ':' on every 2 second
       lc.setLed(0, 4, 0, true);
     }
   }
   else {
     if ( (pvt.second & 0x01) == 0) {
-      lc.setLed(0, 0, 0, false);                    // Blink '.'  every 2 second
+      lc.setLed(0, 3, 0, false);                   // Use DP for Blink '.'  every 2 second
       lc.setLed(0, 4, 0, true);
     }
     else {
-      lc.setLed(0, 0, 0, true);                     // Blink '''  every 2 second
+      lc.setLed(0, 3, 0, true);                    // Use DP for Blink '''  every 2 second
       lc.setLed(0, 4, 0, false);
     }
   }
   // Check ambient light and increase light on display if brighter ambient light
-  LightSensorValue = (LightSensorValue * .8  + (0.2) * constrain(map(analogRead(A1), 650, 1023 , 0 , 19), 0 , 16)); // (int) 15.999999999999999 = 15
-  lc.setIntensity(0, LightSensorValue);            // Set new brightness value display 1
-  lc.setIntensity(1, LightSensorValue);            // Set new brightness value display 2
+  LightSensorValue = (LightSensorValue * .8  + (0.2) * constrain(map(analogRead(A0), 650, 1023 , 0 , 19), 2 , 16)); // (int) 15.999999999999999 = 15
+  for (int z = 0; z < displays; z++) {
+    lc.setIntensity(z, LightSensorValue);          // Set new brightness value display 1
+    delay(100);
+  }
 
-  // write some stuff to the secondary display
-  WriteDisp(pvt.fixType, 1, 7);                    // Write GPS fix value
+  // write some stuff to the secondary display if present
+  WriteDisp(pvt.fixType, 1, 7);                    // Write GPS fix value to display 2
+  if (pvt.numSV < 3 ) {                            // If satelit count > 3 then the time is very correct
+    lc.setLed(0, 1, 0, true);                      // Use DP led for idication of almost correct time
+  }
+  else {
+    lc.setLed(0, 1, 0, false);                     //  DP led OFF for idication of realy correct time
+  }
   WriteDisp(WeekDay(year(), month(), day()), 1, 4);// Write weekday number sun=0, sat=6
   WriteDisp(LightSensorValue, 1, 1);               // Write brightnes value
 
-  if ( DEBUG ) {                               // display output on serial port if debug is set
+  if ( DEBUG ) {                                   // display output on serial port if debug is set
     Serial.print(hour());
     Serial.print(":");
     printDigits(minute());
@@ -295,7 +304,8 @@ void digitalClockDisplay() {
 
 /* Write 2-digit value to 7-segment display
  * PARAMS: value to display, display number, digit number
- * RETURN: - */
+ * RETURN: - 
+ */
 void WriteDisp (int value, int disp, int dig) {
   if ( disp == 0 )if ( dig == 5 )if ( value < 10) {
         lc.setDigit(disp, dig - 1, value, false);
@@ -309,7 +319,8 @@ void WriteDisp (int value, int disp, int dig) {
 /* Calculate Day of Week from the date with Sakamoto's Algorithm
  * https://en.wikipedia.org/wiki/Determination_of_the_day_of_the_week#Sakamoto.27s_algorithm
  * PARAMS: year, month, day
- * RETURN: Day of week, Sun=0, Sat=6 */
+ * RETURN: Day of week, Sun=0, Sat=6 
+ */
 int WeekDay(int y, int m, int d) {
   static int t[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
   y -= m < 3;
@@ -321,7 +332,8 @@ int WeekDay(int y, int m, int d) {
  * Summertime from last sunday in mars 02:00 to last sunday in october 03:00
  * https://www.timeanddate.com/time/change/sweden/stockholm
  * PARAMS: year, month, day of week, Nth occurence of that day from last day in month
- * RETURN: date */
+ * RETURN: date 
+ */
 int NthDate(int y, int m, int DOW, int NthWeek) {
   char Date = 31;
   char firstDOW = WeekDay(y, m, Date);
@@ -334,7 +346,8 @@ int NthDate(int y, int m, int DOW, int NthWeek) {
 
 /* Prints a leding zero to singular digits on Serial
  * PARAMS: Digit to print to serial
- * RETURN: - */
+ * RETURN: - 
+ */
 void printDigits(int digits) {
   // utility function for digital clock display: prints preceding colon and leading 0
   if (digits < 10) {
@@ -345,7 +358,8 @@ void printDigits(int digits) {
 
 /* Calculate checksum of received GPS reading
  * PARAMS: Reading fråm GPS
- * RETURN: -  */
+ * RETURN: -  
+ */
 void calcChecksum(unsigned char* CK) {
   memset(CK, 0, 2);
   for (int i = 0; i < (int)sizeof(NAV_PVT); i++) {
@@ -356,7 +370,8 @@ void calcChecksum(unsigned char* CK) {
 
 /* Read data from GPS
  * PARAMS:
- * RETURN: Reading OK or NotOK  */
+ * RETURN: Reading OK or NotOK  
+ */
 bool processGPS() {
   static int fpos = 0;
   static unsigned char checksum[2];
@@ -395,11 +410,16 @@ bool processGPS() {
   return false;
 }
 
+/* Write messages on display
+ * PARAMS: message number
+ * RETURN: -  
+ */
 void write_msg( int msg_type) {
   switch (msg_type) {
     case 0:
-      /* Write "Err " on display over HH and MM
-       * Used as an error message if GPS time lost */
+      /* Write "Err " on display(0) over HH and MM
+       * Used as an error message if GPS time lost 
+       */
       lc.setChar(0, 5, 'e', false);
       lc.setLed(0, 4, 5, true);
       lc.setLed(0, 4, 7, true);
@@ -408,40 +428,41 @@ void write_msg( int msg_type) {
       lc.setChar(0, 2, ' ', false);
       break;
     case 1:
-      /* Write blinking "helo" on display over HH and MM
-       * Used as an welcome message during initialization */
-      lc.clearDisplay(0); //
+      /* Write blinking "helo" on display(0) over HH and MM
+       * Used as an welcome message during initialization 
+       */
+      lc.clearDisplay(0);
       for (int a = 0; a < 4; a++) {
         lc.setChar(0, 5, 'h', false);
         lc.setChar(0, 4, 'e', false);
         lc.setChar(0, 3, 'l', false);
         lc.setChar(0, 2, '0', false);
         delay(500);
-        lc.setChar(0, 2, ' ', false);
-        lc.setChar(0, 3, ' ', false);
-        lc.setChar(0, 4, ' ', false);
-        lc.setChar(0, 5, ' ', false);
-        delay(500);
         lc.clearDisplay(0);
+        delay(500);
       }
       break;
     case 2:
-      /* Write software Version on display
-       * Used as an welcome message during initialization */
-      lc.clearDisplay(0); //
-      lc.setChar(0, 0, Version[5], false);
-      lc.setChar(0, 1, Version[4], false);
-      lc.setChar(0, 2, Version[3], false);
-      lc.setChar(0, 3, Version[2], false);
-      lc.setChar(0, 4, Version[1], false);
-      lc.setChar(0, 5, Version[0], false);
+      /* Write software Version on display(0)
+       * Message during initialization
+       */
+      lc.clearDisplay(0);
+      for (int i=0; i < 6; i++) {
+        lc.setChar(0, i, Version[5-i], false);
+      }
       delay(2000);
       lc.clearDisplay(0);
       break;
     case 3:
-      /* Lights upp every led i display to check
-       * display during initialization */
+      /* Lights upp every led and DP in display(0,1) to check
+       * display during initialization 
+       */
       lc.clearDisplay(0);
+      lc.clearDisplay(1);
+      for (int y = 5; y > -1; y--) {
+        lc.setLed(0, y, 0, true);
+        delay(150);
+      }
       for (int y = 5; y > -1; y--) {
         lc.setLed(0, y, 6, true);
         lc.setLed(1, y, 6, true);
@@ -475,9 +496,11 @@ void write_msg( int msg_type) {
       break;
     case 4:
 
-      /* Lights upp every led i display to check
-       * display during initialization */
+      /* Lights upp every led in display(0,1) to check
+       * display during initialization 
+       */
       lc.clearDisplay(0);
+      lc.clearDisplay(1);
       for (int y = 0; y < 6; y++) {
         lc.setLed(0, y, 2, true);
         lc.setLed(1, y, 2, true);
